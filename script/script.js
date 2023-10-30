3c8526d1e2551f586369b2cef0cdb3f3d2ba1e29
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2023-10-30 17:29:49 -0300

    .

--- script/script.js
+++ script/script.js
@@ -3,9 +3,14 @@ const cardholderName = document.querySelector('#cardholder-name');
 const spnName = document.querySelector('.name');
 const cardNumber = document.querySelector('#Card-number');
 const txtCard = document.querySelector('.txt-card');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    checkInputCard();
+    checkInputM();
 });
 
 cardholderName.addEventListener('keyup', () => {
@@ -18,8 +23,29 @@ cardNumber.addEventListener('keyup', () => {
     let cardNumberValue = cardNumber.value;
 
     txtCard.innerHTML = cardNumberValue;
-    
 });
+
+// .replace(/\d{4}/g, '')
+
+function checkInputCard() {
+    if (isNumber(cardNumber.value) === false) {
+        setErrorFor(cardNumber, 'Wrong format, numbers only');
+    } else {
+        setSuccessFor(cardNumber);
+    }
+}
+
+function checkInputM() {
+    if (isNumber(month.value) === false) {
+        setErrorFor(month, "Can't be blanck");
+    } else {
+        setSuccessFor(month);
+    }
+}
+
+function isNumber(input) {
+    return /^\d*$/i.test(input);
+  }
 
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
